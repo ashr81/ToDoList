514d601b85a996481c616f0436610c90dfc8aeda
ashr81 <ashrithreddi81@gmail.com>
2015-11-07 19:54:21 +0530

    after 9 pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Profile.destroy_all
+TodoItem.destroy_all
+TodoList.destroy_all
+Profile.create!([{first_name: "Carly" ,last_name: "Fiorina", gender: "female" ,birth_year: 1954},
+{first_name: "Donald" ,last_name: "Trump", gender: "male" ,birth_year: 1946},
+{first_name: "Ben" ,last_name: "Carson", gender: "male" ,birth_year: 1951},
+{first_name: "Hillary" ,last_name: "Clinton", gender: "female" ,birth_year: 1947}])
+
+due_date = Date.today + 1.year
+profiles = Profile.all
+profiles.each do |profile|
+	profile.create_user(username: profile.last_name,
+						password_digest: profile.birth_year)
+	user_details = profile.user.todo_lists.create(list_name: profile.first_name, list_due_date: due_date)
+	5.times do |i|
+		user_details.todo_items.create(due_date: due_date,title: "Coursera#{i}",
+			description:"Deep Dive into Active Records#{i}",completed: false)
+	end
+end
+
+
